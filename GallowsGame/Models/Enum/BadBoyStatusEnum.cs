e8e21ec4d8a334a51b566e1fff18fc32bcb2f934
ierehon13 <razdoru.net@yandex.ru>
2024-01-10 14:56:09 +0300

    +

---
+++ GallowsGame/Models/Enum/BadBoyStatusEnum.cs
@@ -0,0 +1,8 @@
+namespace GallowsGame.Models.Enum;
+
+public enum BadBoyStatusEnum
+{
+    Init = 0,
+    Death = 1,
+    Clear = 2,
+}
--- GallowsGame/Views/Shared/Components/BadBoy/BadBoyViewComponent.cs
+++ GallowsGame/Views/Shared/Components/BadBoy/BadBoyViewComponent.cs
@@ -1,10 +1,11 @@
+using GallowsGame.Models.Enum;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GallowsGame.Views.Shared.Components.BadBoy;
 
 public class BadBoyViewComponent : ViewComponent
 {
-    public async Task<IViewComponentResult> InvokeAsync(bool active = false)
+    public async Task<IViewComponentResult> InvokeAsync(BadBoyStatusEnum active = BadBoyStatusEnum.Init)
     {
         return View(active);
     }

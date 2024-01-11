cf7ce9930d5359e6dd9ee9286944c07af4bf77f5
ierehon13 <razdoru.net@yandex.ru>
2024-01-12 01:54:30 +0300

    +

--- GallowsGame/Model/Requests/GamePostRequest.cs
+++ GallowsGame/Model/Requests/GamePostRequest.cs
@@ -1,6 +1,9 @@
+using BLL.Models.DTO;
+
 namespace GallowsGame.Requests;
 
 public class GamePostRequest
 {
     public string Name { get; set; }
+    public IDictionary<string,IDictionary<string, bool>> Questions { get; set; }
 }
